4e079863604cc21f64eabf0e547c3797f2dc88e5
JiaWeiPls <jlee211@e.ntu.edu.sg>
2020-11-18 14:53:59 +0800

    Optimised code

--- chatbot-watson-android-master/app/src/main/java/com/example/vmac/WatBot/ThermActivity.java
+++ chatbot-watson-android-master/app/src/main/java/com/example/vmac/WatBot/ThermActivity.java
@@ -1,60 +1,22 @@
 package com.example.vmac.WatBot;
 
+import android.animation.ValueAnimator;
+import java.text.DecimalFormat;
+
 import android.content.Intent;
-import android.Manifest;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.graphics.Typeface;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.Toast;
-
-import com.ibm.cloud.sdk.core.http.HttpMediaType;
-import com.ibm.cloud.sdk.core.http.Response;
-import com.ibm.cloud.sdk.core.http.ServiceCall;
-import com.ibm.cloud.sdk.core.security.IamAuthenticator;
-import com.ibm.watson.assistant.v2.Assistant;
-import com.ibm.watson.assistant.v2.model.CreateSessionOptions;
-import com.ibm.watson.assistant.v2.model.DialogNodeOutputOptionsElement;
-import com.ibm.watson.assistant.v2.model.MessageInput;
-import com.ibm.watson.assistant.v2.model.MessageOptions;
-import com.ibm.watson.assistant.v2.model.MessageResponse;
-import com.ibm.watson.assistant.v2.model.RuntimeResponseGeneric;
-import com.ibm.watson.assistant.v2.model.SessionResponse;
-import com.ibm.watson.developer_cloud.android.library.audio.MicrophoneHelper;
-import com.ibm.watson.developer_cloud.android.library.audio.MicrophoneInputStream;
-import com.ibm.watson.developer_cloud.android.library.audio.StreamPlayer;
-import com.ibm.watson.developer_cloud.android.library.audio.utils.ContentType;
-import com.ibm.watson.speech_to_text.v1.SpeechToText;
-import com.ibm.watson.speech_to_text.v1.model.RecognizeOptions;
-import com.ibm.watson.speech_to_text.v1.model.SpeechRecognitionResults;
-import com.ibm.watson.speech_to_text.v1.websocket.BaseRecognizeCallback;
-import com.ibm.watson.text_to_speech.v1.TextToSpeech;
-import com.ibm.watson.text_to_speech.v1.model.SynthesizeOptions;
-
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
+import android.widget.TextView;
 
 public class ThermActivity extends AppCompatActivity {
 
   private ImageButton btnEMF;
   private ImageButton btnRadio;
+  private TextView thermDisplay;
+  private static DecimalFormat df = new DecimalFormat("00.0");
+  public boolean ghostNear = false;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -66,6 +28,14 @@ public class ThermActivity extends AppCompatActivity {
 
       btnEMF = findViewById(R.id.emfButton);
       btnRadio = findViewById(R.id.radioButton);
+      thermDisplay = findViewById(R.id.temperature);
+      //displayFreq();
+      if(!ghostNear) {
+          animateTextView(3.5f, 4.6f, 600000, thermDisplay);
+      } else {
+          animateTextView(5.2f, 56.79f, 60000, thermDisplay);
+      }
+
 
       btnEMF.setOnClickListener(new View.OnClickListener() {
           @Override
@@ -82,6 +52,21 @@ public class ThermActivity extends AppCompatActivity {
               startActivity(intent);
           }
       });
+  }
+  public void animateTextView(float initialValue, float finalValue, int duration, final TextView textview) {
+      ValueAnimator valueAnimator = ValueAnimator.ofFloat(initialValue, finalValue);
+      valueAnimator.setDuration(duration);
+
+      valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+          @Override
+          public void onAnimationUpdate(ValueAnimator valueAnimator) {
+
+              textview.setText(df.format(valueAnimator.getAnimatedValue()));
+
+          }
+      });
+      valueAnimator.start();
+
   }
 }
 
